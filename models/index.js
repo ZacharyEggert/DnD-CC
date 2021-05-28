05cc96f34fa7751d6bccf38b35344d8a84b9322c
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-05-28 15:20:36 -0700

    Empty Files

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Armor = require('./Armor');
+const Character = require('./Character');
+const Class = require('./Class');
+const Effect = require('./Effect');
+const Race = require('./Race');
+const Spell = require('./Spell');
+const Subclass = require('./Subclass');
+const Weapon = require('./Weapon');
+
+
+module.exports = {Armor, Character, Class, Effect, Race, Spell, Subclass, Weapon}
